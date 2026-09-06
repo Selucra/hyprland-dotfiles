fa0242f43c3de82d25f3f363246f0fc579a3ce9c
Selucra <Selucra@users.noreply.github.com>
2026-09-06 14:28:03 +0200

    split hyprland config

---
+++ dotfiles/hypr/configs/env.lua
@@ -0,0 +1,12 @@
+-------------------------------
+---- ENVIRONMENT VARIABLES ----
+-------------------------------
+
+hl.env("XCURSOR_SIZE", "24")
+hl.env("HYPRCURSOR_SIZE", "24")
+hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
+hl.env("XDG_SESSION_TYPE", "wayland")
+hl.env("XDG_SESSION_DESKTOP", "Hyprland")
+hl.env("QT_QPA_PLATFORM", "wayland")
+hl.env("QT_QPA_PLATFORMTHEME", "qt6ct")
+hl.env("EDITOR", "vscodium")
---
+++ dotfiles/hypr/configs/misc.lua
@@ -0,0 +1,24 @@
+----------------
+----  MISC  ----
+----------------
+
+hl.config({
+    misc = {
+        force_default_wallpaper = 0,
+        disable_hyprland_logo = true,
+        disable_splash_rendering = true,
+        allow_session_lock_restore = true,
+    },
+})
+
+hl.config({
+    xwayland = {
+        force_zero_scaling = true
+  }
+})
+
+hl.config({
+    ecosystem = {
+        no_donation_nag = true
+    }
+})
